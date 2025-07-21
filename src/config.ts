436e50325459f3b73b1f30c012734c4eb9a8244a
YYanovich <yurayanovichofficial@gmail.com>
2025-07-21 23:35:12 +0300

    updated files

--- src/config.ts
+++ src/config.ts
@@ -4,6 +4,6 @@ const API_URL = "https://chat-app-backend-production-9b0d.up.railway.app";
 // URL для локальної розробки (якщо знадобиться)
 // const API_URL = "http://localhost:5001";
 // UserChatPage = http://localhost:5001/messages/${userID}
-// AllUsers = http://localhost:5001/users?page=${currentPage}&limit=10&search=${debouncedInputValue}
+/// AllUsers = http://localhost:5001/users?page=${currentPage}&limit=10&search=${debouncedInputValue}
 
 export default API_URL;
