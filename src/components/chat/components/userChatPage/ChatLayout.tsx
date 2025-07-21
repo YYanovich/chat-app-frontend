5ad1fe928bc7d0509d1a6c6010c5712342b883af
YYanovich <yurayanovichofficial@gmail.com>
2025-07-21 23:18:23 +0300

    fix bugs

--- src/components/chat/components/userChatPage/ChatLayout.tsx
+++ src/components/chat/components/userChatPage/ChatLayout.tsx
@@ -3,7 +3,7 @@ import { Box } from "@mui/material";
 import { Socket } from "socket.io-client";
 import AllUsers from "../../components/users/AllUsers";
 import { useTheme } from "../../../../store/hooks";
-import { Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom"; // <-- Важливий імпорт
 
 //компонент для чату з лейаутом
 export default function ChatLayout({ socket }: { socket: Socket }) {
@@ -13,34 +13,24 @@ export default function ChatLayout({ socket }: { socket: Socket }) {
     <Box
       sx={{
         display: "flex",
-        // ВИПРАВЛЕНО: Займає всю доступну висоту
-        height: "calc(100vh - 64px)",
-        background: themeStyles.background,
+        height: "100vh",
       }}
     >
-      {/* Ліва колонка (сайдбар) */}
       <Box
         sx={{
           width: "320px",
           flexShrink: 0,
           borderRight: "1px solid",
           borderColor: "divider",
-          height: "100%",
-          display: "flex",
         }}
       >
         <AllUsers isSidebar={true} />
       </Box>
-
-      {/* Права колонка (чат) */}
       <Box
         sx={{
           flexGrow: 1,
-          height: "100%",
           display: "flex",
           flexDirection: "column",
-          // ВИПРАВЛЕНО: Запобігає виштовхуванню контентом
-          minWidth: 0,
         }}
       >
         <Outlet />
